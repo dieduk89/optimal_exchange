7fe2005d80b8b00e706b5919d16ea99185a9f8f9
Diego Dukon <dieduk89@gmail.com>
2026-03-03 22:15:48 -0400

    add extra case for testing and update terminal output info

--- src/solution.py
+++ src/solution.py
@@ -1,27 +1,36 @@
+
 def solve(case: str) -> str:
     lines = case.split("\n")
     n = int(lines[0])
-    result = ""
+    results = []
     for i in range(n):
+        print("\n") 
         values = list(map(int, lines[i + 1].split(" ")))
         k, size, coins = values[0], values[1], values[2:]
         coins.sort()
         print(f"Item {i + 1}: {k}, {size}, {coins}")
-        print("\n")
         optimal_exchange = find_optimal_exchange(k, size, coins)
-        result += optimal_exchange
-    return result
+        results.append(optimal_exchange) 
+    return "\n".join(results)
 
 def find_optimal_exchange(k: int, size: int, coins: list[int]) -> str:
     optimals_values = [i + 1 if i + 1 not in coins else 1 for i in range(k)]
+
+    #first iteration to find the optimal exchanges for each value
     for i in range(k):
         optimals_values[i] = calculate_optimal_exchanges(optimals_values, i + 1, coins)
-    #print(f"Optimals values: {optimals_values}")
+    #second iteration to find the optimal exchanges for each value
+    for i in range(k):
+        optimals_values[i] = calculate_optimal_exchanges(optimals_values, i + 1, coins) 
+
+    ## in the most cases the optimal exchange is found in the second iteration
+    ## but maybe in some cases it is necessary to iterate more times
+       
     avg_value = round(sum(optimals_values) / len(optimals_values), 2)
     max_value = max(optimals_values)
     print(f"Avg value: {avg_value}") 
     print(f"Max value: {max_value}")
-    return f"{avg_value} {max_value}\n"
+    return f"{avg_value} {max_value}"
 
 
 def calculate_optimal_exchanges(optimals_values: list[int], x: int, coins: list[int]) -> int:
@@ -29,8 +38,6 @@ def calculate_optimal_exchanges(optimals_values: list[int], x: int, coins: list[
     for coin in coins:
         diff = abs(coin - x)
         exchanges.append(optimals_values[diff - 1] + 1)
-    #print(f"Exchanges for {x}: {exchanges}")
-    #print("\n")
     return min(exchanges)
 
    
